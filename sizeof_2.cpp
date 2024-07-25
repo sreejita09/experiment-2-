34a810e6ef0d5cb645f21e7803d772520e15d630
Sreejita Bhardwaj <sreejitabhardwaj2005@gmail.com>
2024-07-25 09:39:21 +0530

    Add files via upload

---
+++ sizeof_2.cpp
@@ -0,0 +1,25 @@
+//Sreejita Bhardwaj 
+//Prn: 23070123130 
+//experiment 3: opertators 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"enter any integer: ";
+    cin>>a;
+    cout<<"\n integer = "<<a<<" and size is "<<sizeof(a)<<" bytes";
+    
+    float b;
+    cout<<"\n enter a number: ";
+    cin>>b;
+    cout<<"\n Float= "<<b<<" and size is "<<sizeof(b)<<" bytes";
+    
+    short int c;
+    cout<<"\nenter an interger: ";
+    cin>>c;
+    cout<<"\nShort integer = "<<c<<" and size is "<<sizeof(c)<<" bytes";
+    return 0;
+}
---
+++ sizeof_3.cpp
@@ -0,0 +1,25 @@
+//Sreejita Bhardwaj 
+//Prn: 23070123130 
+//experiment 3: opertators 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<"enter any integer: ";
+    cin>>a;
+    cout<<"\n integer ="<<a<<" and size is "<<sizeof(a)<<" bytes";
+    
+    int register b;
+    cout<<"\n enter a number: ";
+    cin>>b;
+    cout<<"\n REGISTER= "<<b<<" and size is "<<sizeof(b)<<" bytes";
+    
+    int auto c;
+    cout<<"\n enter an interger: ";
+    cin>>c;
+    cout<<"\n Auto = "<<c<<" and size is "<<sizeof(c)<<" bytes";
+    return 0;
+}
